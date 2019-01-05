3886cfa939d76ee50f561b7da824e6e4c911a22f
Paul Page <pjpage98@gmail.com>
2019-01-04 20:39:38 -0600

    Improve bootstrapping, add git config and ubuntu program list

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,48 +1,22 @@
-dir=~/dotfiles
+dir=$(pwd)
 olddir=~/dotfiles_old
 
 cd ~
 echo "Creating $olddir to backup old dotfiles..."
 mkdir -p $olddir
 
-mv ~/.vimrc $olddir/vimrc
-mv ~/.vim $olddir/vim
-mv ~/.Xresources $olddir/Xresources
-mv ~/.conky $olddir/conky
+mv $HOME/.bashrc $olddir
+mv $HOME/.config/nvim/init.vim $olddir
+mv $HOME/.gitconfig $olddir
 
 echo "Done."
 
 echo
 
-mkdir -p .vim
+mkdir -p $HOME/.config/nvim/autoload
+cd $HOME/.config/nvim/autoload
+curl -LO https://github.com/junegunn/vim-plug/raw/master/plug.vim
 
-echo "Symlinking new dotfiles..."
-ln -s $dir/tmux.conf ~/.tmux.conf
-ln -s $dir/vim/vimrc ~/.vimrc
-ln -s $dir/Xresources ~/.Xresources
-ln -s $dir/vim/snippets ~/.vim/snippets
-ln -s $dir/conky ~/.conky
-echo "Done."
-
-read -p "Use config files for i3? " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	echo "Backing up old files..."
-	mv ~/.config/i3/config $olddir/i3_config
-	mv ~/.xinitrc $olddir/xinitrc
-	echo "Done."
-
-	echo "Symlinking new dotfiles..."
-	ln -s $dir/i3/config ~/.config/i3/config
-	ln -s $dir/i3/xinitrc ~/.xinitrc
-	echo "Done."
-fi
-
-echo "Bootstrapping vim..."
-cd ~/.vim
-mkdir bundle
-cd bundle
-git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git
-vim +PluginInstall +qall
-echo "Done."
+ln -sf $dir/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -sf $dir/bashrc $HOME/.bashrc
+ln -sf $dir/gitconfig $HOME/.gitconfig
---
+++ programs_ubuntu.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt-get install git build-essential neovim-qt rxvt-unicode ffmpeg youtube-dl libxft-dev dmenu feh apt-file libreoffice gnumeric gimp audacity qemu linux-tools-common linux-tools-generic
+
