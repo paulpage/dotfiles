4dd2a7a618e9a9e5d9c28c87bafd30c8f5adc856
Paul Page <pjpage98@gmail.com>
2019-01-28 09:04:45 -0600

    Tracing, file, and DE utils

--- programs_ubuntu.sh
+++ programs_ubuntu.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-sudo apt-get install -y git build-essential neovim-qt rxvt-unicode ffmpeg youtube-dl libxft-dev dmenu feh apt-file libreoffice gnumeric gimp audacity qemu linux-tools-common linux-tools-generic golang-go cargo
+sudo apt-get install -y git build-essential neovim-qt rxvt-unicode ffmpeg youtube-dl libxft-dev dmenu feh apt-file libreoffice gnumeric gimp audacity qemu linux-tools-common linux-tools-generic golang-go cargo bpfcc-tools
 
 # Required for vagrant (the lastest version of vagrant has to be downloaded from the website)
 sudo apt-get install -y virtualbox linux-headers-generic virtualbox-dkms nfs-kernel-server
+
+sudo apt-get install -y exfat-fuse exfat-utils
+
+# DE
+sudo apt-get install -y i3 fonts-font-awesome
